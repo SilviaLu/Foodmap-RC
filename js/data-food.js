9305b1d1e38a1a53af6ae98496520b4827fd735a
SilvaLu <spvv2706@gmail.com>
2018-01-02 11:00:12 -0500

    data.js

--- js/data-food.js
+++ js/data-food.js
@@ -1,11 +1,12 @@
 var peruFood = {
-  'LIMA': {
+  'FOOD': {
     'Comidas': {
       'locales': [
         {
           'name': 'Manos Criollas', // Restaurant N° 1
           'address': 'Av. La Mar 2269, San Miguel 15088',
           'phone': '(01) 4519602',
+          'type': 'comida peruana',
           'photo': 'http://2.bp.blogspot.com/_xuGdHciXE00/TJJZOiLr-HI/AAAAAAAAAdo/RlSG8wYtPWI/s320/local+manos+morenas.jpg',
           'link': 'https://www.manoscriollas.com/',
           'foods': {
@@ -40,6 +41,7 @@ var peruFood = {
           'name': 'Bahía Restaunrant', // Restaurant N° 2
           'address': 'Ucayali 119, San Miguel 15088',
           'phone': '(01) 5622098',
+          'type': 'comida peruana',
           'photo': 'https://media-cdn.tripadvisor.com/media/photo-s/08/86/f9/dc/restaurante-bahia-peru.jpg',
           'link': 'restaurantebahiaperu.com',
           'foods': {
@@ -75,6 +77,7 @@ var peruFood = {
           'name': 'Aromas Peruanos', // Restaurant N° 3
           'address': 'Av. Guardia Civil 856, San Isidro 15036',
           'phone': '(01) 2241482',
+          'type': 'comida peruana',
           'photo': 'http://revistazoom.pe/crop.php?imagen=admin/notas/revista_zoom_218_4.jpg&ancho=800&alto_maximo=450',
           'link': 'aromasperuanos.com',
           'foods': {
@@ -104,21 +107,12 @@ var peruFood = {
             }
             ]
           }
-        }
-      ]
-    }
-  }
-};
-
-
-var italianFood = { 
-  'LIMA': {
-    'Comidas': {
-      'locales': [ 
+        },
         {
           'name': 'Restaurant Danica', // Restaurant N° 1
           'address': 'Av. Emilio Cavenecia 170, San Isidro 15073',
           'phone': '(01)4211891',
+          'type': 'comida italiana',
           'photo': 'http://www.netlima.com/peru/7733.jpg',
           'link': 'http://www.danica.pe/',
           'foods': {
@@ -153,6 +147,7 @@ var italianFood = {
           'name': 'Don Rosalino', // Restaurant N° 2
           'address': 'Av. República de Panamá 5897, Miraflores 15048',
           'phone': '(01)5555555',
+          'type': 'comida italiana',
           'photo': 'https://restorando-res.cloudinary.com/image/upload/s--pzvKKHsn--/c_fit,f_auto,h_500,w_700/v1/restaurant_photos/w/4988/32697/restaurante_don-rosalino-san-borja_san-borja_foto_salon_noche.jpg',
           'link': 'http://www.donrosalino.com/',
           'foods': {
@@ -187,6 +182,7 @@ var italianFood = {
           'name': 'Piacere', // Restaurant N° 3
           'address': 'Av. Salaverry 3100, San Isidro Lima 27',
           'phone': '(01)2644335',
+          'type': 'comida italiana',
           'photo': 'https://media-cdn.tripadvisor.com/media/photo-s/09/d8/9e/94/20151223-130832-largejpg.jpg',
           'link': 'https://www.tripadvisor.com.pe/Restaurant_Review-g294316-d1092141-Reviews-Piacere-Lima_Lima_Region.html',
           'foods': {
@@ -216,20 +212,12 @@ var italianFood = {
             }
             ]
           }
-        }
-      ]
-    }
-  }
-};
-
-var mexicoFood = {
-  'LIMA': {
-    'Comidas': {
-      'locales': [
+        },
         {
           'name': 'Taqueria No Te Rajes', // Restaurant N° 1
           'address': '18, Av. Alfredo Benavides 1083, Miraflores 15047',
           'phone': '959511813',
+          'type': 'comida mexicana',
           'photo': 'https://photo980x880.mnstatic.com/5f/9a/5f9afb3f0db066ce2bb20d313dbe8a48.jpg',
           'link': 'https://www.facebook.com/lostacosnoterajes/',
           'foods': {
@@ -264,6 +252,7 @@ var mexicoFood = {
           'name': 'Mr. Tack', // Restaurant N° 2
           'address': 'Escardo 609, San Miguel 15088',
           'phone': '(01) 3735060',
+          'type': 'comida mexicana',
           'photo': 'https://i.ytimg.com/vi/upmfgVoq1nk/hqdefault.jpg',
           'link': 'mrtack.com',
           'foods': {
@@ -298,6 +287,7 @@ var mexicoFood = {
           'name': 'Chinga tu Taco', // Restaurant N° 3
           'address': 'Av Mariscal La Mar 1300, Miraflores 15074',
           'phone': '(01)5463827',
+          'type': 'comida mexicana',
           'photo': 'http://vexpresa.pe/wp-content/uploads/2017/11/Chinga-tu-taco-15.jpg',
           'link': 'En proceso',
           'foods': {
@@ -332,6 +322,235 @@ var mexicoFood = {
     }
   }
 };
+
+
+// var italianFood = { 
+//   'LIMA': {
+//     'Comidas': {
+//       'locales': [ 
+//         {
+//           'name': 'Restaurant Danica', // Restaurant N° 1
+//           'address': 'Av. Emilio Cavenecia 170, San Isidro 15073',
+//           'phone': '(01)4211891',
+//           'type': 'comida italiana',
+//           'photo': 'http://www.netlima.com/peru/7733.jpg',
+//           'link': 'http://www.danica.pe/',
+//           'foods': {
+//             'main-course': [{
+//               'name': 'Pizza en rollitos',
+//               'photo': 'http://www.comidakraft.com/-/media/assets/recipe_images/rollitos-de-pollo-con-mozzarella-171607-580x326.jpg',
+//               'description': 'Muy jugosos',
+//               'size': 'familiar',
+//               'price': 20.0,
+//               'service': 'Servicio en delivery y local'
+//             },
+//             {
+//               'name': 'Brochetas de queso, albahaca y pimientos',
+//               'photo': 'https://www.recetin.com/wp-content/uploads/2013/11/brochetas.jpg',
+//               'description': 'como guarnicion lleva ensalada y papas doradas',
+//               'size': 'familiar',
+//               'price': 22.5,
+//               'service': 'Servicio en delivery y local'
+//             },
+//             {
+//               'name': 'Lasaña de verduras a la sartén',
+//               'photo': 'http://assets.kraftfoods.com/assets/recipe_images/Skillet-Vegetable-Lasagna-61465.jpg',
+//               'description': 'Con berenjena, calabacita verde y la calabacita amarilla.',
+//               'size': 'familiar',
+//               'price': 25.0,
+//               'service': 'Servicio en delivery y local'
+//             }
+//             ],
+//           }
+//         },
+//         {
+//           'name': 'Don Rosalino', // Restaurant N° 2
+//           'address': 'Av. República de Panamá 5897, Miraflores 15048',
+//           'phone': '(01)5555555',
+//           'type': 'comida italiana',
+//           'photo': 'https://restorando-res.cloudinary.com/image/upload/s--pzvKKHsn--/c_fit,f_auto,h_500,w_700/v1/restaurant_photos/w/4988/32697/restaurante_don-rosalino-san-borja_san-borja_foto_salon_noche.jpg',
+//           'link': 'http://www.donrosalino.com/',
+//           'foods': {
+//             'main-course': [{
+//               'name': 'Pizza Capri',
+//               'photo': 'https://media-cdn.tripadvisor.com/media/photo-s/0a/1b/10/6f/almost-gourmet-pizza.jpg',
+//               'description': 'Lleva jamón, salame, champiñones y esparragos.',
+//               'size': 'familiar',
+//               'price': 23.5,
+//               'service': 'Servicio a deivery y en local'
+//             },
+//             {
+//               'name': 'Pizza Vegetariana',
+//               'photo': 'https://i.ytimg.com/vi/6jNMinJKE40/maxresdefault.jpg',
+//               'description': 'Champiñones, esparragos morrones, aceitunas verdes, y aceituas negras de manera opcional',
+//               'size': 'familiar',
+//               'price': 25.0,
+//               'service': 'Servicio a delivery y en local'
+//             },
+//             {
+//               'name': 'Pizza Quatro quesos',
+//               'photo': 'http://www.quepedimos.net/pizza-hut/files/2015/03/4-quesos.jpg',
+//               'description': 'Preferidas por muchos de nuestro clientes, lleva queso gruyere, gorgonzola, parmesano y mozzarella',
+//               'price': 25.0,
+//               'service': 'Servicio a domicilio y local'
+//             },
+  
+//             ]
+//           }
+//         },
+//         {
+//           'name': 'Piacere', // Restaurant N° 3
+//           'address': 'Av. Salaverry 3100, San Isidro Lima 27',
+//           'phone': '(01)2644335',
+//           'type': 'comida italiana',
+//           'photo': 'https://media-cdn.tripadvisor.com/media/photo-s/09/d8/9e/94/20151223-130832-largejpg.jpg',
+//           'link': 'https://www.tripadvisor.com.pe/Restaurant_Review-g294316-d1092141-Reviews-Piacere-Lima_Lima_Region.html',
+//           'foods': {
+//             'main-course': [{
+//               'name': 'Gnocchis relleno al funghi',
+//               'photo': 'https://unaitalianaenlacocina.es/wp-content/uploads/DSC_3671-1080x675.jpg',
+//               'description': 'Deliciosos rellenos de champiñones, portobello y funghi',
+//               'size': 'para 4 personas',
+//               'price': 25,
+//               'service': 'Servicio exclusivo en local'
+//             },
+//             {
+//               'name': 'Canelones',
+//               'photo': 'http://orsimages.unileversolutions.com/ORS_Images/Knorr_es-ES/Canelones_WEB_OK_33_2.1.132_1214X2164.Jpeg',
+//               'description': 'Con suave relleno a base de lomo fino y pechugas de pollo.',
+//               'size': 'para 3-4 personas',
+//               'price': 26,
+//               'service': 'Servicio exclusivo en local'
+//             },
+//             {
+//               'name': 'Spaguetti a la Marinera',
+//               'photo': 'http://www.cocinadetodo.com/wp-content/uploads/2013/07/espaguetis-con-gambas-620x330.jpg',
+//               'description': 'Con jugosos langotinos y calmares, acompañada de salsa pomorolo!',
+//               'size': 'para 3-4 personas',
+//               'price': 28,
+//               'service': 'Servicio exclusivo en nuestro local de miraflores y san isidro'
+//             }
+//             ]
+//           }
+//         }
+//       ]
+//     }
+//   }
+// };
+
+// var mexicoFood = {
+  // 'LIMA': {
+  //   'Comidas': {
+  //     'locales': [
+  //       {
+  //         'name': 'Taqueria No Te Rajes', // Restaurant N° 1
+  //         'address': '18, Av. Alfredo Benavides 1083, Miraflores 15047',
+  //         'phone': '959511813',
+  //         'type': 'comida mexicana',
+  //         'photo': 'https://photo980x880.mnstatic.com/5f/9a/5f9afb3f0db066ce2bb20d313dbe8a48.jpg',
+  //         'link': 'https://www.facebook.com/lostacosnoterajes/',
+  //         'foods': {
+  //           'main-course': [{
+  //             'name': 'Tacos al Pastor',
+  //             'photo': 'https://www.comedera.com/wp-content/uploads/2017/06/1.jpg',
+  //             'description': 'Tortilla de maíz con piña, cebolla, cilantro y salsa picante.',
+  //             'size': 'familiar',
+  //             'price': 20.0,
+  //             'service': 'Servicio en delivery y local'
+  //           },
+  //           {
+  //             'name': 'Enchiladas de pollo, carne y vegetariana',
+  //             'photo': 'https://www.comedera.com/wp-content/uploads/2017/06/2.jpg',
+  //             'description': 'Ttortillas dobladas y rellenas, bañadas en salsa al gusto.',
+  //             'size': 'familiar',
+  //             'price': 22.5,
+  //             'service': 'Servicio en delivery y local'
+  //           },
+  //           {
+  //             'name': 'Nachos en salsa de Guacamole',
+  //             'photo': 'https://www.comedera.com/wp-content/uploads/2017/06/3.jpg',
+  //             'description': 'Riquisimos nachos clásicos en salsa guacamole con chile.',
+  //             'size': 'familiar',
+  //             'price': 25.0,
+  //             'service': 'Servicio en delivery y local'
+  //           }
+  //           ],
+  //         }
+  //       },
+  //       {
+  //         'name': 'Mr. Tack', // Restaurant N° 2
+  //         'address': 'Escardo 609, San Miguel 15088',
+  //         'phone': '(01) 3735060',
+  //         'type': 'comida mexicana',
+  //         'photo': 'https://i.ytimg.com/vi/upmfgVoq1nk/hqdefault.jpg',
+  //         'link': 'mrtack.com',
+  //         'foods': {
+  //           'main-course': [{
+  //             'name': 'Tamales',
+  //             'photo': 'https://www.comedera.com/wp-content/uploads/2017/06/4.jpg',
+  //             'description': 'Tamales de maiz con riquisima salsa picante, en toques de limon..',
+  //             'size': 'familiar',
+  //             'price': 23.5,
+  //             'service': 'Servicio a deivery y en local'
+  //           },
+  //           {
+  //             'name': 'chilaquiles',
+  //             'photo': 'https://www.comedera.com/wp-content/uploads/2017/06/5.jpg',
+  //             'description': 'Desayuno mexicano que levanta hasta los muertos, a base de pedazos de tortilla de maíz frita.',
+  //             'size': 'familiar',
+  //             'price': 25.0,
+  //             'service': 'Servicio a delivery y en local'
+  //           },
+  //           {
+  //             'name': 'Chiles Rellenos',
+  //             'photo': 'https://www.comedera.com/wp-content/uploads/2017/06/8.jpg',
+  //             'description': ' Rellenos con queso, carne, pollo, atún, pescados, mariscos cubiertos en salsa, gratinados o capeados',
+  //             'price': 25.0,
+  //             'service': 'Servicio a domicilio y local'
+  //           },
+	
+  //           ]
+  //         }
+  //       },
+  //       {
+  //         'name': 'Chinga tu Taco', // Restaurant N° 3
+  //         'address': 'Av Mariscal La Mar 1300, Miraflores 15074',
+  //         'phone': '(01)5463827',
+  //         'type': 'comida mexicana',
+  //         'photo': 'http://vexpresa.pe/wp-content/uploads/2017/11/Chinga-tu-taco-15.jpg',
+  //         'link': 'En proceso',
+  //         'foods': {
+  //           'main-course': [{
+  //             'name': 'Mole Poblano',
+  //             'photo': 'https://www.comedera.com/wp-content/uploads/2017/06/9.jpg',
+  //             'description': 'Llamado también “salsa de chocolate”, comúnmente se come con pavo, pollo o enchiladas',
+  //             'size': 'para 2 personas',
+  //             'price': 25,
+  //             'service': 'Servicio exclusivo en local'
+  //           },
+  //           {
+  //             'name': 'Tostadas',
+  //             'photo': 'https://www.comedera.com/wp-content/uploads/2017/06/10.jpg',
+  //             'description': 'Tortilla de maíz frita, y por encima, los ingredientes que quieras: mariscos, carne molida, pollo, guacamole, y sigue la lista.',
+  //             'size': 'para 2 personas',
+  //             'price': 26,
+  //             'service': 'Servicio exclusivo en local'
+  //           },
+  //           {
+  //             'name': 'Picadillo',
+  //             'photo': 'https://www.comedera.com/wp-content/uploads/2017/06/11.jpg',
+  //             'description': 'Carne molida entomatada cocida con cebolla, ajo, verduras picadas, especias y chiles.',
+  //             'size': 'para 3-4 personas',
+  //             'price': 28,
+  //             'service': 'Servicio exclusivo en nuestro local de miraflores y san isidro'
+  //           }
+  //           ]
+  //         }
+  //       }
+  //     ]
+  //   }
+  // }
+// };
 
 $(document).ready(function() {
   var $btn = $('.btn-footer');
